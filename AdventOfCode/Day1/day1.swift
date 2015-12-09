990b741cfe6aadac7eccf4d05124d4c840a23709
Markus Kauppila <markus.kauppila@gmail.com>
2015-12-09 15:36:22 +0200

    Solve part II of day 1

--- AdventOfCode/Day1/day1.swift
+++ AdventOfCode/Day1/day1.swift
@@ -19,5 +19,34 @@ func day1() {
     let floorsUp = input.filter { $0 == "(" }.count
     let floorsDown = input.filter { $0 == ")" }.count
 
-    print("answer (part 1): \(floorsUp - floorsDown)")
+    let answer = floorsUp - floorsDown
+    print("answer (part 1): \(answer)")
+    assert(answer == 74)
+
+    let position = characterPositionOfDescendingToBasement(input)
+    print("answer (part 2): \(position)")
+    assert(position == 1795)
+}
+
+func characterPositionOfDescendingToBasement(input: Array<Character>) -> Int {
+    var floorLevel = 0
+    var indexOfDescent = 1
+    var index = 1
+
+    for character in input {
+        if character == "(" {
+            floorLevel++
+        } else {
+            floorLevel--
+        }
+
+        if (floorLevel < 0) {
+            indexOfDescent = index
+            break;
+        }
+
+        index++
+    }
+
+    return indexOfDescent
 }
